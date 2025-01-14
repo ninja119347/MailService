cf7dcd5ee667e3e705a5616feb147a2325b29773
taozc1 <1193471306@qq.com>
2025-01-14 20:10:30 +0800

    改成从环境变量中读取config

--- api/dao/Admin.go
+++ api/dao/Admin.go
@@ -4,15 +4,32 @@ package dao
 import (
 	"admin-go-api/api/entity"
 	"admin-go-api/pkg/db"
+	"os"
 )
 
-const (
-	DEFAULT_KEY   = "Default"
-	UNIUPDATE_KEY = "0725#uni#UPDATE"
-	AIMONITOR_KEY = "#ai@monitor0725"
-	SIGN_KEY      = "sign$Lenovo1984"
+// const (
+//
+//	DEFAULT_KEY   = "Default"
+//	UNIUPDATE_KEY = "0725#uni#UPDATE"
+//	AIMONITOR_KEY = "#ai@monitor0725"
+//	SIGN_KEY      = "sign$Lenovo1984"
+//
+// )
+// 常量定义
+var (
+	DEFAULT_KEY   = getEnv("DEFAULT_KEY", "Default")   // 默认密钥
+	UNIUPDATE_KEY = getEnv("UNIUPDATE_KEY", "Default") // UniUpdate密钥
+	AIMONITOR_KEY = getEnv("AIMONITOR_KEY", "Default") // AI监控密钥
+	SIGN_KEY      = getEnv("SIGN_KEY", "Default")      // 签名密钥
 )
 
+// 获取环境变量值，如果未设置则返回默认值
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
 func CheckAppNameExists(appName string) (bool, error) {
 	var count int64
 	err := db.Db.Model(&entity.SysAdmin{}).Where("AppName = ?", appName).Count(&count).Error
--- common/config/config.go
+++ common/config/config.go
@@ -2,6 +2,9 @@ package config
 
 import (
 	"flag"
+	"fmt"
+	"strings"
+
 	//"io/ioutil"
 	"gopkg.in/yaml.v2"
 	"os"
@@ -55,19 +58,52 @@ type log struct {
 
 var Config *config
 
-func LoadConfig(configPath string) {
-	file, err := os.ReadFile(configPath)
+// 读取配置文件并动态替换占位符
+func parseConfigWithEnv(filePath string) error {
+	// 读取文件内容
+	content, err := os.ReadFile(filePath)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("failed to read config file: %v", err)
 	}
-	err1 := yaml.Unmarshal(file, &Config)
-	if err1 != nil {
-		panic(err1)
+
+	// 将内容转换为字符串
+	configStr := string(content)
+
+	// 替换占位符为环境变量值
+	for _, envVar := range os.Environ() {
+		parts := strings.SplitN(envVar, "=", 2)
+		if len(parts) == 2 {
+			placeholder := fmt.Sprintf("{%s}", parts[0]) // {ENV_VAR}
+			configStr = strings.ReplaceAll(configStr, placeholder, parts[1])
+		}
 	}
+
+	// 解析 YAML 内容到 map
+	//var config map[string]interface{}
+	err = yaml.Unmarshal([]byte(configStr), &Config)
+	if err != nil {
+		return fmt.Errorf("failed to parse config: %v", err)
+	}
+	return nil
 }
+
+//func LoadConfig(configPath string) {
+//	file, err := os.ReadFile(configPath)
+//	if err != nil {
+//		panic(err)
+//	}
+//	err1 := yaml.Unmarshal(file, &Config)
+//	if err1 != nil {
+//		panic(err1)
+//	}
+//}
 
 func init() {
 	configPath := flag.String("config", "config_dev.yaml", "path to the config file")
 	flag.Parse()
-	LoadConfig(*configPath)
+	//LoadConfig(*configPath)
+	err := parseConfigWithEnv(*configPath)
+	if err != nil {
+		panic(err)
+	}
 }
