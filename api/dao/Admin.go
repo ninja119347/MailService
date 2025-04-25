538b1b801ba84d854aeeb5df24f40d3f4786b39e
taozc1 <1193471306@qq.com>
2025-04-25 14:10:11 +0800

    更新AUTOBUILD_KEY默认密钥

--- api/dao/Admin.go
+++ api/dao/Admin.go
@@ -17,12 +17,12 @@ import (
 // )
 // 常量定义
 var (
-	DEFAULT_KEY   = getEnv("DEFAULT_KEY", "Default")   // 默认密钥
-	UNIUPDATE_KEY = getEnv("UNIUPDATE_KEY", "Default") // UniUpdate密钥
-	AIMONITOR_KEY = getEnv("AIMONITOR_KEY", "Default") // AI监控密钥
-	SIGN_KEY      = getEnv("SIGN_KEY", "Default")      // 签名密钥
-	MAIL_PORT     = getEnv("MAIL_PORT", "80")          // 邮件发送地址
-	AUTOBUILD_KEY = getEnv("AUTOBUILD_KEY", "Default") // 自动构建密钥
+	DEFAULT_KEY   = getEnv("DEFAULT_KEY", "Default")             // 默认密钥
+	UNIUPDATE_KEY = getEnv("UNIUPDATE_KEY", "Default")           // UniUpdate密钥
+	AIMONITOR_KEY = getEnv("AIMONITOR_KEY", "Default")           // AI监控密钥
+	SIGN_KEY      = getEnv("SIGN_KEY", "Default")                // 签名密钥
+	MAIL_PORT     = getEnv("MAIL_PORT", "80")                    // 邮件发送地址
+	AUTOBUILD_KEY = getEnv("AUTOBUILD_KEY", "sign_Lenovo119347") // 自动构建密钥
 )
 
 // 获取环境变量值，如果未设置则返回默认值
